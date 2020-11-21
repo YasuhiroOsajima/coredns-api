d05357892ecb6da050e8fc22416d36f69d8a7dc2
Yasuhiro Osajima <kimukimu33@gmail.com>
2020-11-21 19:41:10 +0000

    update unittest

--- internal/model/coredns_conf_test.go
+++ internal/model/coredns_conf_test.go
@@ -8,8 +8,9 @@ import (
 func TestGetInfoCoreDNSConf(t *testing.T) {
 	name := "hogehoge.hoge"
 	domainFileInfo := `# DomainUUID: 3e8fc6b1-0a93-4c57-9f2b-95d3e66a66e0
-	172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
-	172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6`
+172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
+172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6
+`
 	domain, err := NewDomain(name, domainFileInfo)
 	if err != nil {
 		t.Error(err)
@@ -43,8 +44,9 @@ func TestGetInfoCoreDNSConf(t *testing.T) {
 func TestAddCoreDNSConf(t *testing.T) {
 	name := "hogehoge.hoge"
 	domainFileInfo := `# DomainUUID: 3e8fc6b1-0a93-4c57-9f2b-95d3e66a66e0
-	172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
-	172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6`
+172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
+172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6
+`
 	domain, err := NewDomain(name, domainFileInfo)
 	if err != nil {
 		t.Error(err)
--- internal/model/domain_test.go
+++ internal/model/domain_test.go
@@ -2,14 +2,16 @@ package model
 
 import (
 	"errors"
+	"strings"
 	"testing"
 )
 
-func TestDomain(t *testing.T) {
+func TestNewDomain(t *testing.T) {
 	name := "hogehoge.hoge"
 	domainFileInfo := `# DomainUUID: 3e8fc6b1-0a93-4c57-9f2b-95d3e66a66e0
-	172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
-	172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6`
+172.21.1.1  hogeserver1.hogehoge.hoge  # 5b9ea8eb-5ce5-422a-9d70-37d25fa896ae
+172.21.1.2  hogeserver2.hogehoge.hoge  # f0c5edcd-3b18-4c26-a8e1-3f3495504dd6
+`
 	domain, err := NewDomain(name, domainFileInfo)
 	if err != nil {
 		t.Error(err)
@@ -21,5 +23,38 @@ func TestDomain(t *testing.T) {
 	}
 	if domain.Name != expect {
 		t.Error(errors.New("NewDomain is not successes"))
+	}
+
+	info, err := domain.GetFileInfo()
+	if err != nil {
+		t.Error(err)
+	}
+	if info != domainFileInfo {
+		t.Error("domainFileInfo is missmatched")
+	}
+}
+
+func TestNewOriginalDomain(t *testing.T) {
+
+	name := "hogehoge.hoge"
+	domain, err := NewOriginalDomain(name)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expect, err := NewDomainName(name)
+	if err != nil {
+		t.Error(err)
+	}
+	if domain.Name != expect {
+		t.Error(errors.New("NewDomain is not successes"))
+	}
+	info, err := domain.GetFileInfo()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !strings.HasPrefix(info, "# DomainUUID: ") {
+		t.Error("domainFileInfo is missmatched")
 	}
 }
